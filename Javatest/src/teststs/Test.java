9cd1fe8df7e27ca7afdb43d5701643058c946b66
sjammala <sjammala@DESKTOP-LGOLIJ3>
2021-10-23 00:14:24 -0700

    4th try

--- Javatest/src/teststs/Test.java
+++ Javatest/src/teststs/Test.java
@@ -5,6 +5,7 @@ public class Test {
 		System.out.println("This is a test to check sts");
 		System.out.println("Check commts ");
 		System.out.println("Third try");
+		System.out.println("fourth try");
 	}
 
 }
