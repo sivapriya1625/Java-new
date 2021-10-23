f93e3e7e0650078064b61fab26f3803f7e3ca344
sjammala <sjammala@DESKTOP-LGOLIJ3>
2021-10-23 00:12:56 -0700

    12:42

--- Javatest/src/teststs/Test.java
+++ Javatest/src/teststs/Test.java
@@ -4,6 +4,7 @@ public class Test {
 	public static void main(String[] args) {
 		System.out.println("This is a test to check sts");
 		System.out.println("Check commts ");
+		System.out.println("Third try");
 	}
 
 }
