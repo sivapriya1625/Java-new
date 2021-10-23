f3ab9d6502a59054ac1452b39086fe4ddbc97c6c
sjammala <sjammala@DESKTOP-LGOLIJ3>
2021-10-23 00:10:56 -0700

    12:40

--- Javatest/src/teststs/Test.java
+++ Javatest/src/teststs/Test.java
@@ -3,6 +3,7 @@ package teststs;
 public class Test {
 	public static void main(String[] args) {
 		System.out.println("This is a test to check sts");
+		System.out.println("Check commts ");
 	}
 
 }
